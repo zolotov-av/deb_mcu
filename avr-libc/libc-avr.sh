590d4f3f8f0bd1c82749ba936d2c89e3502f521d
Alex V. Zolotov <shade@shamangrad.net>
2020-07-16 21:08:49 +0300

    avr-libc

---
+++ avr-libc/libc-avr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+. config
+
+set -x
+
+DEST=$PACKAGES/binutils-avr
+BUILD=$BASEDIR/build
+
+rm -rf $DEST $BUILD
+mkdir -pv $DEST $BUILD
+
+cd $BUILD
+$SRC/avr-libc-$VERSION/configure --prefix=/usr --build=$HOST_ARCH --host=avr
+make $PARALLEL
+make DESTDIR=$DEST install
+
+# cleanup
+#rm -rf $DEST/$TARGET_LIBDIR/*.la
+#rm -rf $DEST/usr/share/info
+
+mkdir -pv $DEST/DEBIAN
+cp -v $BASEDIR/libc-avr.control $DEST/DEBIAN/control
+cd $PACKAGES
+fakeroot dpkg-deb --build $DEST $BASEDIR
