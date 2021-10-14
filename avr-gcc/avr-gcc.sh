5f3a4577b89cffd82098c5fbaaaf5f1813528103
Alex V. Zolotov <shade@shamangrad.net>
2021-10-14 19:10:52 +0300

    fix gcc

--- avr-gcc/avr-gcc.sh
+++ avr-gcc/avr-gcc.sh
@@ -10,12 +10,12 @@ TEMP=$PACKAGES/temp
 DEST=$PACKAGES/avr-gcc
 BUILD=$BASEDIR/build
 
-#rm -rf $DEST $BUILD
-#mkdir -pv $DEST $BUILD
+rm -rf $DEST $BUILD
+mkdir -pv $DEST $BUILD
 
 cd $BUILD
 $SRC/gcc-$VERSION/configure --prefix=/usr --libdir=$HOST_LIBDIR --build=$HOST_ARCH --host=$HOST_ARCH --target=avr --enable-multilib --disable-shared --disable-nls --enable-languages=c,c++ --disable-libssp --with-dwarf2
-make $PARALLEL
+nice make $PARALLEL
 make DESTDIR=$TEMP install-strip
 
 rsync -a $TEMP/ $DEST/
