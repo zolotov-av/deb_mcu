f8372240dfebb1996f680d6bf4ea15ddbec6ae25
Alex V. Zolotov <shade@shamangrad.net>
2021-10-14 19:53:44 +0300

    arm-binutils 2.37

--- arm-binutils/arm-binutils.sh
+++ arm-binutils/arm-binutils.sh
@@ -14,7 +14,7 @@ rm -rf $TEMP $DEST $BUILD
 mkdir -pv $TEMP $DEST $BUILD
 
 cd $BUILD
-$SRC/binutils-$VERSION/configure --prefix=/usr --libdir=$HOST_LIBDIR --build=$HOST_ARCH --host=$HOST_ARCH --target=$ARM_ARCH --enable-multilib --enable-shared --disable-static --disable-nls
+$SRC/binutils-$VERSION/configure --prefix=/usr --libdir=$HOST_LIBDIR --build=$HOST_ARCH --host=$HOST_ARCH --target=$ARM_ARCH --enable-multilib --disable-shared --disable-nls
 make $PARALLEL
 make DESTDIR=$TEMP install-strip
 
@@ -23,6 +23,10 @@ rsync -ac $TEMP/ $DEST/
 # cleanup
 rm -rf $DEST/$HOST_LIBDIR/*.la
 rm -rf $DEST/usr/share/info
+rm -rf $DEST/$HOST_LIBDIR/bfd-plugins/libdep.a
+rmdir $DEST/$HOST_LIBDIR/bfd-plugins
+rmdir $DEST/$HOST_LIBDIR/
+rmdir $DEST/usr/lib
 
 mkdir -pv $DEST/DEBIAN
 cp -v $BASEDIR/arm-binutils.control $DEST/DEBIAN/control
